f9953bd62a21ea01d4fb7d48d625074f821b000d
gimlet-coder <riffle3580@gmail.com>
2025-11-07 19:57:11 +0900

    add

---
+++ GSOUpdate-DeepLLL_partial.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <Eigen/Dense>
+#include <cmath> // std::fabs() (絶対値の計算) std::round() に用いる 
+#include <stdexcept> // std::out_of_range のために必要
+
+// Eigenの型を使いやすく名前を変更した
+using Vector = Eigen::VectorXd;
+using Matrix = Eigen::MatrixXd;
+
+// Eigen::MatrixXd は内部的には double 型
+
+/*memo:
+B.row(i) 行列 B の i 番目の行をひとつの行ベクトルとして取り出す
+B.col(j) 行列 B の j 番目の列をひとつの列ベクトルとして取り出す
+hoge.dot(huga) は hoge と huga の内積を表す
+hoge.squaredNorm() はベクトルの長さの2乗を表す
+*/
+
+void GSOUpdate_DeepLLL_partial(Matrix &U, Vector &B_norm, const int i_in, const int k_in){
+    Vector P = B_norm, D = B_norm;
+    int n = U.rows();
+    const int i = i_in - 1, k = k_in - 1; // 0-index と 1-index の混同を防ぐためにずらした
+
+    for (int j = k - 1; j >= i; j--){
+        P(j) = U(k, j) * B_norm(j);
+        D(j) = D(j + 1) + U(k, j) * P(j);
+    }
+    Vector S(n);
+    S.setZero(); // 初期化 アルゴリズムの6行目の処理
+    for (int j = k; j >= i + 1; j--){
+        double T = U(k, j - 1) / D(j);
+        for (int l = n - 1; l >= k + 1; l--){
+            S(l) += U(l, j - 1) * P(j - 1);
+            U(l, j - 1) = U(l, j - 2) - T * S(l);
+        }
+        for (int l = k - 1; l >= j; l--){
+            S(l) += U(l - 1, j - 1) * P(j - 1);
+            U(l, j - 1) = U(l - 1, j - 2) - T * S(l);
+        }
+    }
+    double T = 1.0/D(i);
+/* ここまでで alg.16 */
+    for (int l = n - 1; l >= k + 1; l--){
+        U(l, i) = T * (S(l) + U(l, i) * P(i));
+    }
+    for (int l = k - 1; l >= i + 1; l--){
+        U(l, i) = T * (S(l) + U(l - 1, i) * P(i));
+    }
+    U(i, i - 1) = T * P(i - 1);
+    for (int j = 0; j <= i - 2; j++){
+        double temp = U(k, j);
+        for (int l = k - 1; l >= i; l--){
+            U(l + 1, j) = U(l, j);
+        }
+        U(i, j) = temp;
+    }
+    for (int j = k - 1; j >= i; j--){
+        B_norm(j) = D(j) * B_norm(j - 1) / D(j - 1);
+    }
+    B_norm(i) = D(i);
+}
