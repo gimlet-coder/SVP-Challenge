9f1232fe6261fd93be9de64ad03647a4bf2b7246
gimlet-coder <riffle3580@gmail.com>
2025-11-07 17:44:53 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <cmath> // std::fabs() (絶対値の計算) std::round() に用いる 
 #include <stdexcept> // std::out_of_range のために必要
 
+#include <random> // 10^7 以上の疑似乱数を生成するために必要
+
 #include <windows.h> //　文字化け対策
 
 // Eigenの型を使いやすく名前を変更した
@@ -25,27 +27,41 @@ hoge.squaredNorm() はベクトルの長さの2乗を表す
 
 #include "main.hpp"
 
+// LLL の動作確認
 int main() {
     SetConsoleOutputCP(65001); // このコードの出力文字コードを UTF-8 に強制
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+
+    std::random_device rd; // 乱数のシード値を取得
+    std::mt19937 gen(rd()); //MT法に基づいた乱数エンジン
+
+    int min_val = 10'000'000, max_val = 100'000'000; // 10~7 ~ 10^8 と指定
+
+    std::uniform_int_distribution<int> dist(min_val, max_val);
     
-    Matrix B(3, 3);
-    B << 9, 2, 7,
-         8, 6, 1,
-         3, 2, 6;
+    Eigen::MatrixXi B_int(20, 20);
+
+    B_int.setZero(); // B の初期化
+    for (int i = 0; i < 20; i++){
+        B_int(i, i) = 1;
+        B_int(i, 0) = dist(gen);
+    }
     
-    std::cout << "入力した基底行列 B:\n" << B << std::endl << std::endl;
+    std::cout << "入力した基底行列 B:\n" << B_int << std::endl << std::endl;
     double delta = 0.99;
 
+    Eigen::MatrixXd B_double = B_int.cast<double>(); // LLL関数に入れるため double にキャストしたコピーを作成
     try{
-        LLL(B, delta);
-        std::cout << "簡約パラメータ delta = " << delta << " によってLLL基底簡約した B:\n" << B << std::endl << std::endl;
+        LLL(B_double, delta);
+        
+        B_int = B_double.array().round().cast<int>(); // round で数値誤差を丸めて int にキャストして値を戻す
+
+        std::cout << "簡約パラメータ delta = " << delta << " によってLLL基底簡約した B:\n" << B_double << std::endl << std::endl;
     }
     catch(const std::out_of_range &orr){
         std::cerr << orr.what() << std::endl;
         return 1; // エラー終了
     }
-
     return 0;
 }
