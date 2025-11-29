5d46daafea8761e586bf7996224081ff3ed37f67
gimlet-coder <riffle3580@gmail.com>
2025-11-29 21:21:06 +0900

    update

--- main.cpp
+++ main.cpp
@@ -32,24 +32,31 @@ int main() {
 
     int min_val = 1'000'000, max_val = 10'000'000; // 乱数範囲
 
-    int n = 40; // 格子次元
+     int n = 4; // 格子次元
     
     std::uniform_int_distribution<int> dist(min_val, max_val); // 上記の乱数範囲に応じたランダムな整数を生成する関数
    
-    Eigen::MatrixXi B_int(n, n);
+    /* Eigen::MatrixXi B_int(n, n);
     B_int.setZero();
     for (int i = 0; i < n; i++){
         B_int(i, i) = 1; // 対角成分を 1 に設定
         B_int(i, 0) = dist(gen); // 1列目に大きな乱数を設定
     }
-   
+   */
+    Eigen::MatrixXi B_int(n, n);
+
+    B_int << 388, 417, 417, -86,
+            -672, -73, -121, 944,
+            -689, 379, 724, 653,
+            -179, 96, -24, 978;
+    
    
 
     std::cout << "入力した基底行列 B:\n" << B_int << std::endl << std::endl;
 
     Scalar Norm_input = B_int.row(0).cast<Scalar>().squaredNorm();
 
-    double delta = 0.99;
+    double delta = 0.75;
 
 
 
@@ -57,12 +64,12 @@ int main() {
     B_long_double = B_int.cast<typename Matrix::Scalar>(); // Matrix::Scalarはboost::multiprecision型
 
     try{
-        DeepLLL(B_long_double, delta);
+        LLL(B_long_double, delta);
 
         auto B_tmp_ld = B_long_double.cast<long double>();
         B_int = B_tmp_ld.array().round().cast<int>();
         
-        std::cout << "簡約パラメータ delta = " << delta << " によってDeepLLL基底簡約した B:\n" 
+        std::cout << "簡約パラメータ delta = " << delta << " によってLLL基底簡約した B:\n" 
                     << std::fixed << std::setprecision(0) // 整数表示 (浮動小数点数演算を使用しているため)
                      << B_long_double << std::endl << std::endl;
     }
@@ -76,7 +83,7 @@ int main() {
 
 
 // 出力(B_long_double)の最初の行の長さ
-    std::cout << "DeepLLL後: " << B_long_double.row(0).squaredNorm() << std::endl;
+    std::cout << "LLL後: " << B_long_double.row(0).squaredNorm() << std::endl;
     return 0;
 }
 
@@ -100,6 +107,7 @@ int main() {
     
     Eigen::MatrixXi B_int(5, 4);
 
+    // 例5.2.1
     B_int << 388, 417, 417, -86,
             -672, -73, -121, 944,
             -689, 379, 724, 653,
@@ -142,3 +150,4 @@ int main() {
 }
 
 #endif
+
