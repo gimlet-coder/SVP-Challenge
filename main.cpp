78593ec60c1567bcd8b36792d121cf21f63ce519
gimlet-coder <riffle3580@gmail.com>
2025-12-04 20:19:29 +0900

    update

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 
 #include <windows.h> //　文字化け対策
 #include <iomanip> // 表示桁数を確保するのに使う std::fixed << std::setprecision(0)
+#include <chrono> // 計算時間計測するために使う
 
 #include "lattice_types.hpp"
 // 上記のヘッダーによって Vector と Matrix は Scalar 型になっている
@@ -20,7 +21,7 @@
 #include "main.hpp"
 
 
-#if 1
+#if 0
 
 int main() {
     SetConsoleOutputCP(65001);
@@ -97,7 +98,7 @@ int main() {
 #endif
 
 
-#if 0
+#if 1
 // DeepLLLの動作確認用
 int main() {
     SetConsoleOutputCP(65001); // このコードの出力文字コードを UTF-8 に強制
@@ -107,25 +108,19 @@ int main() {
     std::random_device rd; // 乱数のシード値を取得
     std::mt19937 gen(rd()); //MT法に基づいた乱数エンジン
 
-    int min_val = 1'000'000, max_val = 10'000'000; // 乱数範囲
+    int min_val = 1'000'000, max_val = 100'000'000; // 乱数範囲
 
-     int n = 4; // 格子次元
+     int n = 150; // 格子次元
     
     std::uniform_int_distribution<int> dist(min_val, max_val); // 上記の乱数範囲に応じたランダムな整数を生成する関数
    
-    /* Eigen::MatrixXi B_int(n, n);
+    Eigen::MatrixXi B_int(n, n);
     B_int.setZero();
     for (int i = 0; i < n; i++){
         B_int(i, i) = 1; // 対角成分を 1 に設定
         B_int(i, 0) = dist(gen); // 1列目に大きな乱数を設定
     }
-   */
-    Eigen::MatrixXi B_int(n, n);
 
-    B_int << 388, 417, 417, -86,
-            -672, -73, -121, 944,
-            -689, 379, 724, 653,
-            -179, 96, -24, 978;
     
    
 
@@ -141,14 +136,23 @@ int main() {
     B_long_double = B_int.cast<typename Matrix::Scalar>(); // Matrix::Scalarはboost::multiprecision型
 
     try{
-        LLL(B_long_double, delta);
+        auto start_time = std::chrono::high_resolution_clock::now(); // ここから計測を始める
+        DeepLLL(B_long_double, delta);
+        auto end_time = std::chrono::high_resolution_clock::now(); // 終了時間を保存
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time); // 差分で所要時間を求める
+
+
 
         auto B_tmp_ld = B_long_double.cast<long double>();
         B_int = B_tmp_ld.array().round().cast<int>();
         
-        std::cout << "簡約パラメータ delta = " << delta << " によってLLL基底簡約した B:\n" 
+        std::cout << "簡約パラメータ delta = " << delta << " によってDeepLLL基底簡約した B:\n" 
                     << std::fixed << std::setprecision(0) // 整数表示 (浮動小数点数演算を使用しているため)
                      << B_long_double << std::endl << std::endl;
+
+        std::cout << "\n--- DeepLLL 実行時間 ---\n";
+        std::cout << "実行時間: " << duration.count() << " ms (" << static_cast<double>(duration.count()) / 1000.0 << " s)\n";
+        std::cout << "------------------------\n\n";
     }
     catch(const std::out_of_range &orr){
         std::cerr << orr.what() << std::endl;
@@ -156,11 +160,12 @@ int main() {
     }
 
     std::cout << "--------------------------------" << std::endl;
+    
     std::cout << "簡約前後の最初のベクトルの長さ比較 (2乗ノルム):" << Norm_input <<std::endl;
 
 
 // 出力(B_long_double)の最初の行の長さ
-    std::cout << "LLL後: " << B_long_double.row(0).squaredNorm() << std::endl;
+    std::cout << "DeepLLL後: " << B_long_double.row(0).squaredNorm() << std::endl;
     return 0;
 }
 
