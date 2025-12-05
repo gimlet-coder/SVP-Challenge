1e0e925d60568c492330250d3641b6d6eefa6e16
gimlet-coder <riffle3580@gmail.com>
2025-12-05 20:46:20 +0900

    Upgrade: ENUMの枝刈りに対応

--- BKZ.hpp
+++ BKZ.hpp
@@ -35,7 +35,7 @@ void LLL(Matrix &B, const Scalar delta);
  * @param delta [in] MLLLにおける簡約パラメータ 変更無し
  */
 
- bool ENUM(const Matrix &U, const Vector &B_norm, Scalar &R_square , Vector &v_out, const int k_begin, const int k_end, long long &node_count);
+ bool ENUM(const Matrix &U, const Vector &B_norm, const Vector &R_squares , Vector &v_out, const int k_begin, const int k_end, long long &node_count);
 
  /** 
   * @brief 格子上の最短ベクトルの数え上げ ENUM 
@@ -44,7 +44,7 @@ void LLL(Matrix &B, const Scalar delta);
   *     false 見つからなかった場合
   * @param U [in] GSO係数行列 mu_{i, j} が U(i - 1, j - 1) に格納されていることに注意 変更無し
   * @param B_norm [in] GSOベクトルの2乗ノルム 変更無し
-  * @param R_square [in] 数え上げ上界列 R_n^2 = R_square と考える 関数内で更新していくので変更あり
+  * @param R_squares [in] 数え上げ上界列 枝刈りをして小さくしていく 変更無し
   * @param v_out [out] 条件を満たす格子ベクトルの係数ベクトル 
   * @param k_begin, k_end [in] 探索範囲 変更無し
   * @param node_count [out] 最短ベクトルを探すのにかかったノード数を保存する
